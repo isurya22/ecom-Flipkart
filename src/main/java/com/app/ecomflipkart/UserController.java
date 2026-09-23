8e3befc216276e3cb9d17a823416fb795e12501e
surya <suryanarayan939@gmail.com>
2026-09-23 23:53:38 +0530

    Added ResponseEntity in all endpoints

--- src/main/java/com/app/ecomflipkart/UserController.java
+++ src/main/java/com/app/ecomflipkart/UserController.java
@@ -1,6 +1,8 @@
 package com.app.ecomflipkart;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,18 +14,23 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/api/getListOfUsers")
-    public List<User> getAllUsers(){
-        return userService.fetchAllUsers();
+    public ResponseEntity<List<User>> getAllUsers(){
+        return new ResponseEntity<>(userService.fetchAllUsers(), HttpStatus.OK);
+        //return ResponseEntity.ok(userService.fetchAllUsers());
     }
 
     @GetMapping("/api/getUser/{id}")
-    public User getUserById(@PathVariable Long id){
-        return userService.getUser(id);
+    public ResponseEntity<User> getUserById(@PathVariable Long id){
+        User user = userService.getUser(id);
+        if(user == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(user);
     }
 
     @PostMapping("/api/createUser")
-    public String createUser(@RequestBody User user){
+    public ResponseEntity<String> createUser(@RequestBody User user){
         userService.addUser(user);
-        return "User created Successfully";
+        return ResponseEntity.ok("User created Successfully");
     }
 }
