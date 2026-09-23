60f155be189f5602fababe19dcc0f72834d839a5
surya <suryanarayan939@gmail.com>
2026-09-23 23:34:53 +0530

    Added getUserById endpoint

--- src/main/java/com/app/ecomflipkart/UserController.java
+++ src/main/java/com/app/ecomflipkart/UserController.java
@@ -1,10 +1,8 @@
 package com.app.ecomflipkart;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -16,6 +14,11 @@ public class UserController {
     @GetMapping("/api/getListOfUsers")
     public List<User> getAllUsers(){
         return userService.fetchAllUsers();
+    }
+
+    @GetMapping("/api/getUser/{id}")
+    public User getUserById(@PathVariable Long id){
+        return userService.getUser(id);
     }
 
     @PostMapping("/api/createUser")
--- src/main/java/com/app/ecomflipkart/UserService.java
+++ src/main/java/com/app/ecomflipkart/UserService.java
@@ -1,6 +1,7 @@
 package com.app.ecomflipkart;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.ArrayList;
@@ -13,6 +14,15 @@ public class UserService {
 
     public List<User> fetchAllUsers(){
         return userList;
+    }
+
+    public User getUser(Long id){
+        for(User user : userList){
+            if (user.getId().equals(id)){
+                return user;
+            }
+        }
+        return null;
     }
 
     public void addUser(User user){
