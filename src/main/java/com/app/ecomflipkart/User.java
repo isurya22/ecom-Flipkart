5e970b452493750cb889845521c4bdb6dec57df0
surya <suryanarayan939@gmail.com>
2026-09-24 22:54:28 +0530

    Created UserRepository.java and updated User as Entity

--- src/main/java/com/app/ecomflipkart/User.java
+++ src/main/java/com/app/ecomflipkart/User.java
@@ -1,9 +1,20 @@
 package com.app.ecomflipkart;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+//@Entity(name = "user_table")
+//@AllArgsConstructor
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
